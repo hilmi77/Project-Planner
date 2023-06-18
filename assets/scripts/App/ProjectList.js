eae77a38963607599fa351e6c30227bcc978f38b
Hilmi KALE <120317069+hilmi77@users.noreply.github.com>
2023-06-18 22:53:25 +0300

    projectList add

--- assets/scripts/App/ProjectList.js
+++ assets/scripts/App/ProjectList.js
@@ -1,41 +1,76 @@
-import Cmp, { doSomething } from "./Component.js";
+import { ProjectItem as PrjItem } from "./ProjectItem.js";
+import * as DOMH from "../Utility/DOMHelper.js";
 
-console.log("Tooltip running");
+const ProjectItem = "abc";
 
-export class Tooltip extends Cmp {
-  constructor(closeNotifierFunction, text, hostElementId) {
-    super(hostElementId);
-    this.closeNotifier = closeNotifierFunction;
-    this.text = text;
-    this.create();
+// console.log(DEFAULT_VALUE);
+
+export class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    this.type = type;
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new PrjItem(prjItem.id, this.switchProject.bind(this), this.type)
+      );
+    }
+    console.log(this.projects);
+    this.connectDroppable();
   }
 
-  closeTooltip = () => {
-    this.detach();
-    this.closeNotifier();
-  };
+  connectDroppable() {
+    console.log(globalThis);
+    const list = document.querySelector(`#${this.type}-projects ul`);
 
-  create() {
-    const tooltipElement = document.createElement("div");
-    tooltipElement.className = "card";
-    const tooltipTemplate = document.getElementById("tooltip");
-    const tooltipBody = document.importNode(tooltipTemplate.content, true);
-    tooltipBody.querySelector("p").textContent = this.text;
-    tooltipElement.append(tooltipBody);
+    list.addEventListener("dragenter", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        list.parentElement.classList.add("droppable");
+        event.preventDefault();
+      }
+    });
 
-    const hostElPosLeft = this.hostElement.offsetLeft;
-    const hostElPosTop = this.hostElement.offsetTop;
-    const hostElHeight = this.hostElement.clientHeight;
-    const parentElementScrolling = this.hostElement.parentElement.scrollTop;
+    list.addEventListener("dragover", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+    });
 
-    const x = hostElPosLeft + 20;
-    const y = hostElPosTop + hostElHeight - parentElementScrolling - 10;
+    list.addEventListener("dragleave", (event) => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove("droppable");
+      }
+    });
 
-    tooltipElement.style.position = "absolute";
-    tooltipElement.style.left = x + "px"; // 500px
-    tooltipElement.style.top = y + "px";
+    list.addEventListener("drop", (event) => {
+      const prjId = event.dataTransfer.getData("text/plain");
+      if (this.projects.find((p) => p.id === prjId)) {
+        return;
+      }
+      document
+        .getElementById(prjId)
+        .querySelector("button:last-of-type")
+        .click();
+      list.parentElement.classList.remove("droppable");
+      // event.preventDefault(); // not required
+    });
+  }
 
-    tooltipElement.addEventListener("click", this.closeTooltip);
-    this.element = tooltipElement;
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject(project) {
+    this.projects.push(project);
+    DOMH.moveElement(project.id, `#${this.type}-projects ul`);
+    project.update(this.switchProject.bind(this), this.type);
+  }
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
